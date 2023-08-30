543a6dfa0eb24b6c0c6a8cd37ed1dc6550354d46
estherniyo <estherniyonkuru123@gmail.com>
2023-08-30 11:59:21 +0200

    seting headers

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log("request made")
-  // res.writable("yes")
+// the difference between setHeaders and writeHeaders is that writeHeaders can be used to set the status code 
+// along side with the object containing a number of headers as well
+  // res.setHeader('Content-Type', 'text/plain') this comes like normal response from backends with black background
+  res.setHeader('Content-Type', 'text/html')
+  res.write("Response just sent") // this is to be sent to the client as a part of the response body
+  res.write('<h1> Why not </h1>') //this comes like normal html but you still have to wrap everything like a string
+  res.end() // this is to mark a signal that the response is complete
 });
 
 const port = 3000;
